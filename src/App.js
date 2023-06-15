7e3bff94aa806dd8da8798de5b62303feb0423e6
Amirreza Aminian <aminian.amirreza@gmail.com>
2023-06-15 13:22:18 +0330

    Deleting an Item More Child-to-Parent Communication!

--- src/App.js
+++ src/App.js
@@ -9,16 +9,19 @@ const initialItems = [
 export default function App() {
   const [items, setItems] = useState([]);
 
-   function handleAddItems(item) {
-     setItems((items) => [...items, item]);
-   }
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
 
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
 
   return (
     <div className="app">
       <Logo />
-      <Form onAddItems={handleAddItems}/>
-      <PackingList  items ={items}/>
+      <Form onAddItems={handleAddItems} />
+      <PackingList items={items} onDeleteItem={handleDeleteItem} />
       <Stats />
     </div>
   );
@@ -67,25 +70,25 @@ function Form({ onAddItems }) {
   );
 }
 
-function PackingList({items}) {
+function PackingList({ items, onDeleteItem }) {
   return (
     <div className="list">
       <ul>
         {items.map((item) => (
-          <Item item={item} key={item.id} />
+          <Item item={item} onDeleteItem={onDeleteItem} key={item.id} />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item , onDeleteItem }) {
   return (
     <li>
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
